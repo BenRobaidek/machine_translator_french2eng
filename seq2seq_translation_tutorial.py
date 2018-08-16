b9a758c8843d39ba814a740e7b1ab655ba0db7ae
Ben Robaidek <brobaidek@gmail.com>
2018-08-15 17:35:22 -0700

    matplotlib backend switch

--- seq2seq_translation_tutorial.py
+++ seq2seq_translation_tutorial.py
@@ -669,9 +669,11 @@ def trainIters(encoder, decoder, n_iters, print_every=1000, plot_every=100, lear
 # ``plot_losses`` saved while training.
 #
 
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
-plt.switch_backend('Agg')
-#plt.switch_backend('agg')
+#plt.switch_backend('Agg')
+plt.switch_backend('agg')
 import matplotlib.ticker as ticker
 import numpy as np
 
